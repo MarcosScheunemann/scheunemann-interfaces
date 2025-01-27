1ddfbc52fa9d4233438f42ff10583af60361af98
Marcos <vergueiro81@gmail.com>
2025-01-27 19:52:24 -0300

    email settings

--- src/global-settings/interfaces/i-email-settings.ts
+++ src/global-settings/interfaces/i-email-settings.ts
@@ -1,13 +1,15 @@
-import { EGlobalSettingsType } from "../enums/global-settings-type.enum";
+import { EGlobalSettingsType } from '../enums/global-settings-type.enum';
 
 export interface IEmailSettings {
-    active: boolean;
-    createdAt: Date;
-    email: string;
-    id: string;
-    masterKey: string;
-    pass: string;
-    service: string;
-    type: EGlobalSettingsType;
-    updatedAt: Date;
+  active: boolean;
+  createdAt: Date;
+  email: string;
+  id: string;
+  masterKey: string;
+  pass: string;
+  service: string;
+  type: EGlobalSettingsType;
+  updatedAt: Date;
+  host: string;
+  port: number;
 }
