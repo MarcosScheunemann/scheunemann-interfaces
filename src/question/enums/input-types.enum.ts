fb7950592b7805d421c91f681f8d1df1eea777a1
Marcos <vergueiro81@gmail.com>
2025-01-29 17:36:36 -0300

    atulizado input types

--- src/question/enums/input-types.enum.ts
+++ src/question/enums/input-types.enum.ts
@@ -1,11 +1,44 @@
 export enum EInputTypes {
-  AUTOR = 'text',
-  AUTOR_OPICIONAL = 'boolean',
-  REU = 'text',
-  FORO = 'text',
-  VALOR_DA_CAUSA = 'text',
-  OPCAO_PELA_AUDIENCIA_DE_CONCILIACAO = 'text',
-  LOCAL_E_DATA = 'text',
-  ADVOGADO = 'text',
-  OAB_N = 'text',
+  // Dados do Autor
+  AUTOR = 'AUTOR',
+  // Dados do Réu
+  REU = 'REU',
+  // Dados do Foro
+  FORO = 'FORO',
+  // Dados da Origem
+  ORIGEM = 'ORIGEM',
+  // Valor da causa
+  VALOR_DA_CAUSA = 'VALOR_DA_CAUSA',
+  // Opção pela audiência de conciliação
+  OPCAO_PELA_AUDIENCIA_DE_CONCILIACAO = 'OPCAO_PELA_AUDIENCIA_DE_CONCILIACAO',
+  // Local e data
+  LOCAL_E_DATA = 'LOCAL_E_DATA',
+  // Dados do Advogado
+  ADVOGADO = 'ADVOGADO',
+  // OAB nº
+  OAB_N = 'OAB_N',
+  // Dados do Impetrante
+  IMPETRANTE = 'IMPETRANTE',
+  // Dados do Impetrado
+  IMPETRADO = 'IMPETRADO',
+  // Dados do Embargante
+  EMBARGANTE = 'EMBARGANTE',
+  // Dados do Embargado
+  EMBARGADO = 'EMBARGADO',
+  // Dados do Excipiente
+  EXCIPIENTE = 'EXCIPIENTE',
+  // Dados do Excepto
+  EXCEPTO = 'EXCEPTO',
+  // Dados do Recorrente
+  RECORRENTE = 'RECORRENTE',
+  // Dados do Recorrido
+  RECORRIDO = 'RECORRIDO',
+  // Dados do Agravante
+  AGRAVANTE = 'AGRAVANTE',
+  // Dados do Agravado
+  AGRAVADO = 'AGRAVADO',
+  // Dados do Apelante
+  APELANTE = 'APELANTE',
+  // Dados do Apelado
+  APELADO = 'APELADO',
 }
