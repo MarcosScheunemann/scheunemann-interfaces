8fd07f7d59c729889aa75060b29f794ebf0bd1e6
Marcos <vergueiro81@gmail.com>
2025-02-11 21:31:26 -0300

    esqueci do exportar o preferences

--- src/meli/interfaces/index.ts
+++ src/meli/interfaces/index.ts
@@ -4,3 +4,4 @@ export { IMeliPayerPhone } from './i-meli-payer-phone';
 export { IMeliPreferenceItem } from './i-meli-preference-item';
 export { IMeliPreferencePayerPayment } from './i-meli-preference-payer-payment';
 export { IMeliPreferencePayer } from './i-meli-preference-payer';
+export { IMeliPreferences } from './i-meli-preferences';
