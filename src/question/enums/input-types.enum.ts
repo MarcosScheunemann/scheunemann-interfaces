40f22d68d6e1c2879a3825c8ba1470d7d137e75e
Marcos <vergueiro81@gmail.com>
2025-04-02 20:43:14 -0300

    refactor: enhance EInputTypes enum with detailed comments and additional input types

--- src/question/enums/input-types.enum.ts
+++ src/question/enums/input-types.enum.ts
@@ -1,44 +1,162 @@
 export enum EInputTypes {
-  // Dados do Autor
+  /**
+   *  Dados do Autor
+   **/
   AUTOR = 'AUTOR',
-  // Dados do Réu
+  /**
+   *  Dados do Réu
+   **/
   REU = 'REU',
-  // Dados do Foro
-  FORO = 'FORO',
-  // Dados da Origem
+  /**
+   *  Dados do Foro Contencioso
+   **/
+  FORO_CONTENCIOSO = 'FORO_CONTENCIOSO',
+  /**
+   *  Dados do Foro Consultivo
+   **/
+  FORO_CONSULTIVO = 'FORO_CONSULTIVO',
+  /**
+   *  Dados da Origem
+   **/
   ORIGEM = 'ORIGEM',
-  // Valor da causa
+  /**
+   *  Valor da causa
+   **/
   VALOR_DA_CAUSA = 'VALOR_DA_CAUSA',
-  // Opção pela audiência de conciliação
+  /**
+   *  Opção pela audiência de conciliação
+   **/
   OPCAO_PELA_AUDIENCIA_DE_CONCILIACAO = 'OPCAO_PELA_AUDIENCIA_DE_CONCILIACAO',
-  // Local e data
+  /**
+   *  Local e data
+   **/
   LOCAL_E_DATA = 'LOCAL_E_DATA',
-  // Dados do Advogado
+  /**
+   *  Dados do Advogado
+   **/
   ADVOGADO = 'ADVOGADO',
-  // OAB nº
+  /**
+   *  OAB nº
+   **/
   OAB_N = 'OAB_N',
-  // Dados do Impetrante
+  /**
+   *  Dados do Impetrante
+   **/
   IMPETRANTE = 'IMPETRANTE',
-  // Dados do Impetrado
+  /**
+   *  Dados do Impetrado
+   **/
   IMPETRADO = 'IMPETRADO',
-  // Dados do Embargante
+  /**
+   *  Dados do Embargante
+   **/
   EMBARGANTE = 'EMBARGANTE',
-  // Dados do Embargado
+  /**
+   *  Dados do Embargado
+   **/
   EMBARGADO = 'EMBARGADO',
-  // Dados do Excipiente
+  /**
+   *  Dados do Excipiente
+   **/
   EXCIPIENTE = 'EXCIPIENTE',
-  // Dados do Excepto
+  /**
+   *  Dados do Excepto
+   **/
   EXCEPTO = 'EXCEPTO',
-  // Dados do Recorrente
+  /**
+   *  Dados do Recorrente
+   **/
   RECORRENTE = 'RECORRENTE',
-  // Dados do Recorrido
+  /**
+   *  Dados do Recorrido
+   **/
   RECORRIDO = 'RECORRIDO',
-  // Dados do Agravante
+  /**
+   *  Dados do Agravante
+   **/
   AGRAVANTE = 'AGRAVANTE',
-  // Dados do Agravado
+  /**
+   *  Dados do Agravado
+   **/
   AGRAVADO = 'AGRAVADO',
-  // Dados do Apelante
+  /**
+   *  Dados do Apelante
+   **/
   APELANTE = 'APELANTE',
-  // Dados do Apelado
+  /**
+   *  Dados do Apelado
+   **/
   APELADO = 'APELADO',
+  /**
+   *  Tema
+   **/
+  TEMA = 'TEMA',
+  /**
+   *  Dados dos Queralantes
+   **/
+  QUERELANTES = 'QUERELANTES',
+  /**
+   *  Dados dos Queralados
+   **/
+  QUERELADOS = 'QUERELADOS',
+  /**
+   *  Rol de testemunhas
+   **/
+  ROL_DE_TESTEMUNHAS = 'ROL_DE_TESTEMUNHAS',
+  /**
+   *  Dados do Testemunhante
+   **/
+  TESTEMUNHANTE = 'TESTEMUNHANTE',
+  /**
+   *  Dados do Testemunhado
+   **/
+  TESTEMUNHADO = 'TESTEMUNHADO',
+  /**
+   *  Dados do Requerente
+   **/
+  REQUERENTE = 'REQUERENTE',
+  /**
+   *  Dados do Requerido
+   **/
+  REQUERIDO = 'REQUERIDO',
+  /**
+   *  Dados do Assistente de Acusação
+   **/
+  ASSISTENTE_DE_ACUSACAO = 'ASSISTENTE_DE_ACUSACAO',
+  /**
+   *  Dados do Acusado
+   **/
+  ACUSADO = 'ACUSADO',
+  /**
+   *  Dados do Endereçamento
+   **/
+  ENDERECAMENTO = 'ENDERECAMENTO',
+  /**
+   *  Dados do Representante
+   **/
+  REPRESENTANTE = 'REPRESENTANTE',
+  /**
+   *  Dados do Locador
+   **/
+  LOCADOR = 'LOCADOR',
+  /**
+   *  Dados do Locatário
+   **/
+  LOCATARIO = 'LOCATARIO',
+  /**
+   *  Dados do Fiador
+   **/
+  FIADOR = 'FIADOR',
+  /**
+   *  Dados do Exequente
+   **/
+  EXEQUENTE = 'EXEQUENTE',
+  /**
+   *  Dados do Executado
+   **/
+  EXECUTADO = 'EXECUTADO',
+  /**
+   *  Dados do Tribunal
+   **/
+  TRIBUNAL = 'TRIBUNAL',
 }
