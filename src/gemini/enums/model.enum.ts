eae74793e5553fab036e8d44f997170ed78ae0ea
Marcos <vergueiro81@gmail.com>
2026-02-08 15:00:20 -0300

    add gemini 3.0 flash

--- src/gemini/enums/model.enum.ts
+++ src/gemini/enums/model.enum.ts
@@ -1,4 +1,5 @@
 export enum EChatGeminiModel {
   GEMINI_2_0_FLASH_001 = 'gemini-2.0-flash-001',
+  GEMINI_3_FLASH_PREVIEW = 'gemini-3-flash-preview',
   GEMINI_2_5_FLASH_PREVIEW_001 = 'gemini-2.5-flash-preview-04-17',
 }
